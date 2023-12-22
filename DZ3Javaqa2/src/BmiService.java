965950885599d7699c3bb4370089b9122d67b4ef
Evgeniya <lozinskaya.ev@gmail.com>
2023-12-22 13:05:24 +0300

    change Main.java

---
+++ DZ3Javaqa2/src/BmiService.java
@@ -0,0 +1,7 @@
+public class BmiService {
+
+    public int calculate(double heightMeter, double weightKilogram) {
+        double bmiIndex = weightKilogram / (heightMeter * heightMeter);
+        return (int) bmiIndex;
+    }
+}
--- DZ3Javaqa2/src/Main.java
+++ DZ3Javaqa2/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        BmiService service = new BmiService();
+
+        double heightMeter = 1.87;
+        double weightKilogram = 98;
+
+        int bmiIndex = service.calculate(heightMeter, weightKilogram);
+        System.out.println("Ваш индекс массы тела: " + bmiIndex);
     }
 }
